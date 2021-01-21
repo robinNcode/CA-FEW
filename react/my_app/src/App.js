1bc9ef1267f87da8bb50eb62fc6d39e02b931875
robinNcode <msmrobin518@gmail.com>
2021-01-22 03:42:25 +0600

    menu components added

--- react/my_app/src/App.js
+++ react/my_app/src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
       <Navbar dark color="dark">
         <div className="container">
-          <NavbarBrand href="/">React</NavbarBrand>
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
